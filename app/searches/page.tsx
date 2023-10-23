96f35e31dbfa4e5a6510761153168d2de771a141
Artur Watanabe <arturwatanabe@gmail.com>
2023-10-23 15:53:35 -0300

    fix: searches page

--- app/searches/page.tsx
+++ app/searches/page.tsx
@@ -68,9 +68,9 @@ export default function MySearches() {
   []);
 
   return (
-    <main className="w-full flex-col  bg-primaryGreen flex items-center justify-center relative min-h-[100vh] ">
+    <main className="w-full flex-col   bg-primaryGreen flex items-center justify-center relative min-h-[100vh] py-32 ">
           <NavBar />
-      <div className="flex flex-col  w-[65%] justify-between items-center flex-wrap gap-5 lg:w-full lg:py-10">
+      <div className="flex flex-col  w-[65%] items-center  flex-wrap gap-5 lg:w-full lg:py-10">
         <div className="flex flex-col gap-2">
           <h3 className="font-bold text-white">Buscar por moeda:</h3>
           <select
