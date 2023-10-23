be43cb927afacf3002fa040ec5eb549c82a5f4eb
Artur Watanabe <arturwatanabe@gmail.com>
2023-10-23 16:07:53 -0300

    fix: searches layout

--- app/searches/page.tsx
+++ app/searches/page.tsx
@@ -68,7 +68,7 @@ export default function MySearches() {
   []);
 
   return (
-    <main className="w-full flex-col   bg-primaryGreen flex items-center justify-center relative min-h-[100vh] py-32 ">
+    <main className="w-full flex-col bg-primaryGreen flex items-center relative min-h-[100vh] py-32 ">
           <NavBar />
       <div className="flex flex-col  w-[65%] items-center  flex-wrap gap-5 lg:w-full lg:py-10">
         <div className="flex flex-col gap-2">
